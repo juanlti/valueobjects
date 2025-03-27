b4f757cb407d1dd2842edaebcc843ab93c13c0f2
juanc <juancruzliberati@hotmail.com>
2025-03-27 16:05:21 -0300

    estructura basica de ValueObject para cualquier dominio

---
+++ app/ValueObjects/ValueObject.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\ValueObjects;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+
+abstract class ValueObject implements Arrayable
+{
+
+
+    abstract public function value();
+
+    public static function make(mixed ...$value): static
+    {
+
+        return new static(...$value);
+    }
+
+    public static function from(mixed ...$value): static
+    {
+        //from lo utilizamos para crear una instancia desde los modelos, ejemplo: Producto
+        return static::make(...$value);
+    }
+
+    public function equals(ValueObject $valueObject): bool
+    {
+        return $this->value() === $valueObject->value();
+    }
+
+    public function noEquals(ValueObject $valueObject): bool
+    {
+        return !$this->equals($valueObject);
+    }
+
+    public function toArray(): array
+    {
+        return (array)$this->value();
+    }
+
+    public function toString(): string
+    {
+        return (string)$this->value();
+    }
+    public function __toString()
+    {
+        return $this->toString();
+    }
+
+    public function __set(string $name, mixed $value):void
+    {
+        throw new \Exception("Los ValueObjects son inmutables por lo tanto su valor se puede cambiar.");
+    }
+
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use App\Models\Product;
+use App\Models\User;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Customer::create([
+            'name' => 'John',
+            'lastname' => 'Doe Smith',
+            'email' => 'john@doe.com',
+            'phone' => '512752531',
+            'address' => '123 Main St',
+            'website' => 'https://johndoe.com',
+            'active' => true,
+        ]);
+
+        Customer::create([
+            'name' => 'Jane',
+            'lastname' => 'Doe Smith',
+            'email' => 'jane@doe.com',
+            'phone' => '846438818',
+            'address' => '321 Main St',
+            'website' => 'https://janedoe.com',
+            'active' => true,
+        ]);
+
+        Customer::create([
+            'name' => 'John',
+            'lastname' => 'Smith Doe',
+            'email' => 'john@smith.com',
+            'phone' => '172453858',
+            'address' => '123 Main St',
+            'website' => 'https://johnsmith.com',
+            'active' => true,
+        ]);
+
+        Product::create([
+            'name' => 'Product 1',
+            'price' => 10.99,
+            'image' => 'products/1.jpg',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            'stock' => 10,
+            'active' => true,
+        ]);
+
+        Product::create([
+            'name' => 'Product 2',
+            'price' => 20.99,
+            'image' => 'products/2.jpg',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            'stock' => 20,
+            'active' => true,
+        ]);
+
+        Product::create([
+            'name' => 'Product 3',
+            'price' => 30.99,
+            'image' => 'products/3.jpg',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            'stock' => 30,
+            'active' => true,
+        ]);
+
+        Product::create([
+            'name' => 'Product 4',
+            'price' => 40.99,
+            'image' => 'products/4.jpg',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            'stock' => 40,
+            'active' => true,
+        ]);
+    }
+}
