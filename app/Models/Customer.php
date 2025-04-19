1fb1ec37348c6564730c75f277f5c8e14ae9982e
juanc <juancruzliberati@hotmail.com>
2025-04-19 00:28:37 -0300

    Add Email value object with self-validation

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,16 +2,22 @@
 
 namespace App\Models;
 
+use App\Casts\Email;
 use App\Casts\Name;
-use FullName;
+
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    //cuando se accede a un atributo, automaticamente se crea  un valueObjectEmail
+    // $firstCustomer = Customer::find(1);
+    //$email = firstCustomer->email
+    //$email es no string sino que es un valueObject de la clase Email
     protected $casts = [
         'name' => Name::class,
         'lastname' => Name::class,
+        'email' => Email::class,
     ];
 
     public function fullname(): Attribute
